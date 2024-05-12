f1155933a9bb36203168af379e7c77a729a70544
Raghava Reddy N <raghau.20003@gmail.com>
2024-05-12 23:02:21 +0530

    first commit

---
+++ components/Navber.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navber = () => {
+  return (
+    <div className="flex py-3 flex-wrap justify-around">
+        <h1 className="text-lg font-semiboald">Todo-App</h1>
+        <ul className='flex gap-[40px] font-m'>
+            <li>Home</li>
+            <li>Products</li>
+            <li>About</li>
+            <li>Contact us</li>
+        </ul>
+        </div>
+  )
+}
+
+export default Navber
---
+++ components/TodoList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const TodoList = () => {
+  return (
+    <tr>
+    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">
+    1
+  </th>
+  <td class="px-6 py-4">
+    Project
+  </td>
+  <td class="px-6 py-4">
+  Complete this Next.js Project
+  </td>
+  <td class="px-6 py-4">
+    Pending
+  </td>
+  <td class="px-6 py-4 flex gap-1">
+   <button className='br-5 py-2 px-4 bg-red-500 text-white rounded'>Delete</button>
+   <button className='py-2 px-4 bg-green-500 text-white rounded'>Done</button>
+  </td>
+  </tr>
+  )
+}
+
+export default TodoList
