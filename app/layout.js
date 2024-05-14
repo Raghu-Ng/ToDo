64246ce8b15e43ab4e556ed7e66b4f2710ae2d6f
Raghava Reddy N <raghau.20003@gmail.com>
2024-05-14 08:37:22 +0530

    zz

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navber from "../components/Navber.js";
+import Navber from "../components/Navber";
 
 const inter = Inter({ subsets: ["latin"] });
 
